c2ef27f61b4e09c3ac1fe911ef963fb03520d361
Jake McGinty <me@jake.su>
2021-10-22 13:14:34 +0900

    add more examples to blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -6,23 +6,27 @@ module.exports = {
     // The base URL of the blog itself
     baseUrl: 'https://tonari-notion-blog-demo.web.app',
 
-    // The URL of your GoatCounter instance
+    // The base URL of your GoatCounter instance, ex. 'https://counter.mydomain.com'
     goatCounterUrl: '',
 
     // The footer/copyright statement
-    footer: '©2021 whoever',
+    footer: 'Ⓐ 2021 who cares',
 
-    // The main blog's Open Graph image.
+    // The main blog's Open Graph image. These must me fully qualified URLs, ex. 'https://mydomain.com/og_image.jpg'
     ogImage: '',
+
+    // A twitter handle to associate with the blog, ex. '@everybodycaresaboutme'
     ogTwitter: '',
 
-    // The notion ID of the database containing the blog posts.
+    // The notion ID of the database containing the blog posts. (currente example is the public template)
     databaseId: 'e7cd4a4665904791ac9ee1a351eabe5d',
 
     // Set specific text colors for each author.
-    // Author names are their "suggified" selves (lowercased, with spaces as '-')
+    // Author names are their "sluggified" selves (lowercased, with spaces as '-')
+    // ex. '{ "jake-mcginty": "#ff0000" }'
     authorColors: {},
 
     // Used when a user account is disabled/missing to override the name.
+    // ex. '{ "1d6bfff-ffff-ffff-ffff-ffffffffffff": "Jake McGinty" }
     authorNameOverrides: {}
 }
